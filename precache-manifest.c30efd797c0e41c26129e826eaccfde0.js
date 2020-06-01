2d05a87d99ae0d53287ada064741cbf803d8a454
Rishik <rishikgoyal00@gmail.com>
2020-06-01 19:05:30 +0530

    Updates

---
+++ precache-manifest.c30efd797c0e41c26129e826eaccfde0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b02529b7d4aaa5014618d3950e7441",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "76bed1abc4a0f650425b",
+    "url": "/RoboFriends/static/css/main.989ab452.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/RoboFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bed1abc4a0f650425b",
+    "url": "/RoboFriends/static/js/main.a634e88f.chunk.js"
+  },
+  {
+    "revision": "af258257253a0ffa7f4f",
+    "url": "/RoboFriends/static/js/runtime-main.288d7cdc.js"
+  },
+  {
+    "revision": "b6e9bb37f4e5cd1059e6ebb846a6d40e",
+    "url": "/RoboFriends/static/media/SEGA LOGO FONT.b6e9bb37.woff"
+  }
+]);
